65a2fcd88a149c7ca4abd514ede088b899cfc326
Ilija Studen <ilija.studen@gmail.com>
2024-09-29 17:40:32 +0200

    Add bundles to the Retro package

---
+++ src/Bootstrapper/Bundle/Bundle.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the ActiveCollab Retro project.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Retro\Bootstrapper\Bundle;
+
+abstract class Bundle implements BundleInterface
+{
+    private ?string $name = null;
+
+    public function getName(): string
+    {
+        if ($this->name === null) {
+            $bits = explode('\\', $this::class);
+
+            $this->name = $this->removeBundleSuffix($bits[count($bits) - 1]);
+        }
+
+        return $this->name;
+    }
+
+    private function removeBundleSuffix(string $bundleName): string
+    {
+        if (str_ends_with($bundleName, 'Bundle')) {
+            return substr($bundleName, 0, strlen($bundleName) - 6);
+        }
+
+        return $bundleName;
+    }
+}
---
+++ src/Bootstrapper/Bundle/BundleInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the ActiveCollab Retro project.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Retro\Bootstrapper\Bundle;
+
+interface BundleInterface
+{
+    const PATH = __DIR__;
+    const DEPENDENCIES = __DIR__ . '/dependencies.php';
+
+    public function getName(): string;
+}
