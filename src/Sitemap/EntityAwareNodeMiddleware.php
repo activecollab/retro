90afd12d634cf85a96f19935760d2732c6f7a6a5
Ilija Studen <ilija.studen@gmail.com>
2024-10-06 14:09:58 +0200

    Add entty aware node middleware

---
+++ src/Sitemap/EntityAwareNodeMiddleware.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the ActiveCollab Retro project.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Retro\Sitemap;
+
+use ActiveCollab\DatabaseObject\Entity\EntityInterface;
+use ActiveCollab\DatabaseObject\PoolInterface;
+use Doctrine\Inflector\InflectorFactory;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Interfaces\RouteInterface;
+
+trait EntityAwareNodeMiddleware
+{
+    protected function getEntityFromRequest(
+        ServerRequestInterface $request,
+        string $entityType,
+        string $routeArgumentName = null
+    ): ?EntityInterface
+    {
+        $entityId = (int) $this
+            ->getRoute($request)
+            ->getArgument(
+                $routeArgumentName ?? $this->getIdArgumentNameFromEntityType($entityType)
+            );
+
+        if ($entityId) {
+            $entity = $this->getContainer()
+                ->get(PoolInterface::class)
+                ->getById($entityType, $entityId);
+
+            if ($entity instanceof $entityType) {
+                return $entity;
+            }
+        }
+
+        return null;
+    }
+
+    private function getIdArgumentNameFromEntityType(string $entityType): string
+    {
+        $bits = explode('\\', $entityType);
+
+        return InflectorFactory::create()->build()->tableize($bits[count($bits) - 1]) . '_id';
+    }
+
+    abstract public function &getContainer(): ?ContainerInterface;
+    abstract protected function getRoute(ServerRequestInterface $request): RouteInterface;
+}
