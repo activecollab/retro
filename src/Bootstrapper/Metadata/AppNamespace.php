9ca78d2642c1ce5685e929a8840de64e31bf54ff
Ilija Studen <ilija.studen@gmail.com>
2024-10-06 12:02:28 +0200

    Fix class name

--- src/Bootstrapper/Metadata/AppNamespace.php
+++ src/Bootstrapper/Metadata/AppNamespace.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 
 namespace ActiveCollab\Retro\Bootstrapper\Metadata;
 
-class AppAppNamespace implements AppNamespaceInterface
+class AppNamespace implements AppNamespaceInterface
 {
     public function __construct(private string $appNamespace)
     {
