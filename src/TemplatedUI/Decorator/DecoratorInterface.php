845f02c77e4bd5a099b6721af0272e4eb38db166
Ilija Studen <ilija.studen@gmail.com>
2024-11-06 17:30:14 +0100

    Add missing comment

--- src/TemplatedUI/Decorator/DecoratorInterface.php
+++ src/TemplatedUI/Decorator/DecoratorInterface.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * This file is part of the ActiveCollab Retro project.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ */
+
 declare(strict_types=1);
 
 namespace ActiveCollab\Retro\TemplatedUI\Decorator;
