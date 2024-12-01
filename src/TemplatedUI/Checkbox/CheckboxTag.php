fbc0069456e1dc0377ad09574dcf57d760564135
Ilija Studen <ilija.studen@gmail.com>
2024-12-01 20:20:44 +0100

    Add checkbox tag

---
+++ src/TemplatedUI/Checkbox/CheckboxTag.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the ActiveCollab Retro project.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Retro\TemplatedUI\Checkbox;
+
+use ActiveCollab\Retro\TemplatedUI\Property\Size;
+use ActiveCollab\TemplatedUI\Tag\Tag;
+
+class CheckboxTag extends Tag
+{
+    public function render(
+        string $name,
+        string $label,
+        bool $checked = false,
+        mixed $value = '1',
+        ?string $helpText = null,
+        ?Size $size = null,
+    ): string
+    {
+        $attributes = [
+            'name' => $name,
+            'value' => $value,
+        ];
+
+        if ($checked) {
+            $attributes['checked'] = true;
+        }
+
+        if ($helpText) {
+            $attributes['helpText'] = $helpText;
+        }
+
+        if ($size) {
+            $attributes['size'] = $size->toAttributeValue();
+        }
+
+        return sprintf(
+            '%s%s%s',
+            $this->openHtmlTag(
+                'sl-checkbox',
+                $attributes,
+            ),
+            $this->sanitizeForHtml($label),
+            $this->closeHtmlTag('sl-checkbox'),
+        )
+    }
+}
