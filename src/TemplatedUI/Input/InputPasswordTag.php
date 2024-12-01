34d40f3454ffe04529de4c6ef6f305af9843cc31
Ilija Studen <ilija.studen@gmail.com>
2024-12-01 20:25:38 +0100

    Add input password

---
+++ src/TemplatedUI/Input/InputPasswordTag.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the ActiveCollab Retro project.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Retro\TemplatedUI\Input;
+
+use ActiveCollab\Retro\FormData\FormDataInterface;
+use ActiveCollab\Retro\TemplatedUI\Property\Size;
+use ActiveCollab\TemplatedUI\MethodInvoker\CatchAllParameters\CatchAllParametersInterface;
+
+class InputPasswordTag extends InputTag
+{
+    public function render(
+        string $name,
+        string $placeholder = null,
+        ?Size $size = null,
+        ?FormDataInterface $formData = null,
+        ?CatchAllParametersInterface $catchAllParameters = null,
+    ): string
+    {
+        return $this->renderInput(
+            'password',
+            $name,
+            null,
+            $placeholder,
+            $size,
+            $catchAllParameters,
+            $formData,
+        );
+    }
+}
