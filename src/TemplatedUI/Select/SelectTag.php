37b4d6d58b324a7714dc01e524cf47ebc39026e8
Ilija Studen <ilija.studen@gmail.com>
2025-04-04 07:18:12 +0200

    Add base select tag

---
+++ src/TemplatedUI/Select/SelectTag.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the Feud project.
+ *
+ * (c) PhpCloud.org Core Team <core@phpcloud.org>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Retro\TemplatedUI\Select;
+
+use ActiveCollab\Retro\FormData\FormDataInterface;
+use ActiveCollab\Retro\TemplatedUI\Form\FormField\FormFieldInterface;
+use ActiveCollab\Retro\TemplatedUI\Form\FormField\FormFieldTag;
+
+abstract class SelectTag extends FormFieldTag
+{
+    protected function openSelectTag(
+        string $name,
+        string $class = '',
+    ): string
+    {
+        $classes = [];
+
+        return $this->openHtmlTag(
+            'select',
+            $this->withInjectPlaceholder(
+                [
+                    'name' => $name,
+                    'class' => $this->prepareClassAttribute($class, ...$classes),
+                ],
+            ),
+        );
+    }
+
+    protected function openOptionalSelectTag(
+        string $name,
+        ?FormDataInterface $formData = null,
+        string $class = '',
+        string $width = FormFieldInterface::WIDTH_FULL,
+    ): string
+    {
+        return sprintf(
+            '%s%s%s',
+            $this->openSelectTag($name, $class, $width),
+            $this->renderOption('None', '', empty($formData?->getFieldValue($name))),
+            $this->renderOption(''),
+        );
+    }
+
+    protected function renderOptionGroup(string $label): string
+    {
+        return $this->openHtmlTag(
+            'optgroup',
+            [
+                'label' => $this->sanitizeForHtml($label),
+            ],
+        );
+    }
+
+    protected function closeOptionGroup(): string
+    {
+        return $this->closeHtmlTag('optgroup');
+    }
+
+    protected function renderOption(
+        string $text,
+        string|int $value = '',
+        bool $isSelected = false,
+    ): string
+    {
+        return sprintf(
+            '%s%s%s',
+            $this->openHtmlTag(
+                'option',
+                [
+                    'value' => (string) $value,
+                    'selected' => $isSelected,
+                ],
+            ),
+            $this->sanitizeForHtml($text),
+            $this->closeHtmlTag('option'),
+        );
+    }
+}
