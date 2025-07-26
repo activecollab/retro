fde4cfcd4706c17c96adc3e4d553ff30aa591ce9
Ilija Studen <ilija.studen@gmail.com>
2025-07-26 19:35:12 +0200

    Add delete entity action

---
+++ src/UI/Action/DeleteEntity.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the ActiveCollab Retro project.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Retro\UI\Action;
+
+class DeleteEntity implements ActionInterface
+{
+    public function __construct(
+        private string $deleteUrl,
+        private ?string $confirmMessage = 'Are you sure you want to delete this item?',
+    )
+    {
+    }
+
+    public function extendAttributes(array $attributes): array
+    {
+        $attributesToAppend = [
+            'hx-delete' => $this->deleteUrl,
+        ];
+
+        if ($this->confirmMessage) {
+            $attributesToAppend['hx-confirm'] =  $this->confirmMessage;
+        }
+
+        return array_merge($attributes, $attributesToAppend);
+    }
+}
