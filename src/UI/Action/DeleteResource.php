26e051580306ceea8479d1568b67dc6e3f7285ad
Ilija Studen <ilija.studen@gmail.com>
2025-07-26 19:35:31 +0200

    Rename action

--- src/UI/Action/DeleteResource.php
+++ src/UI/Action/DeleteResource.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 
 namespace ActiveCollab\Retro\UI\Action;
 
-class DeleteEntity implements ActionInterface
+class DeleteResource implements ActionInterface
 {
     public function __construct(
         private string $deleteUrl,
