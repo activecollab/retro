1996c6921912362e4a5554a7b5749122834b1bc2
Ilija Studen <ilija.studen@gmail.com>
2025-08-04 21:47:38 +0200

    Add icon tag

---
+++ src/TemplatedUI/Indicator/IconTag.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the ActiveCollab Retro project.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Retro\TemplatedUI\Indicator;
+
+use ActiveCollab\Retro\UI\Indicator\Icon;
+use ActiveCollab\Retro\UI\Renderer\RendererInterface;
+use ActiveCollab\TemplatedUI\Tag\Tag;
+
+class IconTag extends Tag
+{
+    public function __construct(
+        private RendererInterface $renderer,
+    )
+    {
+    }
+
+    public function render(
+        string $iconName,
+    )
+    {
+        return (new Icon($iconName))->renderUsingRenderer($this->renderer);
+    }
+}
