09b25ddd99bb2b0def7b4938f2adf433585fb92f
Ilija Studen <ilija.studen@gmail.com>
2025-08-15 18:42:59 +0200

    Add info box block

---
+++ src/TemplatedUI/Indicator/InfoBoxBlock.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the ActiveCollab Retro project.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Retro\TemplatedUI\Indicator;
+
+use ActiveCollab\Retro\UI\Common\Variant;
+use ActiveCollab\Retro\UI\Indicator\Icon;
+use ActiveCollab\Retro\UI\Indicator\InfoBox;
+use ActiveCollab\Retro\UI\Renderer\RendererInterface;
+use ActiveCollab\TemplatedUI\WrapContentBlock\WrapContentBlock;
+
+class InfoBoxBlock extends WrapContentBlock
+{
+    public function __construct(
+        private RendererInterface $renderer,
+    )
+    {
+    }
+
+    public function render(
+        string $content,
+        Variant|string $variant = Variant::PRIMARY,
+        ?string $iconName = null,
+    ): string
+    {
+        return (new InfoBox(
+            $content,
+            $variant,
+            $iconName ? new Icon($iconName) : null,
+        ))->renderUsingRenderer($this->renderer);
+    }
+}
