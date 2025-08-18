37142e6ea1ada3f3b79f17411f2c70f4522ddec2
Ilija Studen <ilija.studen@gmail.com>
2025-08-18 08:20:36 +0200

    Test if size is propertly set

--- test/src/UI/Shoelace/RadioGroupTest.php
+++ test/src/UI/Shoelace/RadioGroupTest.php
@@ -12,6 +12,7 @@ namespace ActiveCollab\Retro\Test\UI\Shoelace;
 
 use ActiveCollab\Retro\TemplatedUI\ComponentIdResolver\ComponentIdResolverInterface;
 use ActiveCollab\Retro\Test\Base\TestCase;
+use ActiveCollab\Retro\UI\Common\Size;
 use ActiveCollab\Retro\UI\Form\Radio\Radio;
 use ActiveCollab\Retro\UI\Form\Radio\RadioGroup;
 use ActiveCollab\Retro\UI\Renderer\Shoelace\ShoelaceRenderer;
@@ -32,16 +33,17 @@ class RadioGroupTest extends TestCase
     public function testWilLRender(): void
     {
         $renderedRadioGroup = $this->renderer->renderRadioGroup(
-            new RadioGroup(
+            (new RadioGroup(
                 'radio-group-name',
                 'Radio Group Label',
                 54321,
                 new Radio('Option 1', 12345, true),
-            ),
+            ))->size(Size::LARGE),
         );
 
         $this->assertStringStartsWith('<sl-radio-group', $renderedRadioGroup);
         $this->assertStringEndsWith('</sl-radio-group>', $renderedRadioGroup);
+        $this->assertStringContainsString('size="large"', $renderedRadioGroup);
         $this->assertStringContainsString('radio-group-name', $renderedRadioGroup);
         $this->assertStringContainsString('Radio Group Label', $renderedRadioGroup);
         $this->assertStringContainsString('54321', $renderedRadioGroup);
