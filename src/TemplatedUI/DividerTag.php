0d71ad031ab833927b8617529decd94fdcc21662
Ilija Studen <ilija.studen@gmail.com>
2025-08-25 08:03:46 +0200

    Render a divider

--- src/TemplatedUI/DividerTag.php
+++ src/TemplatedUI/DividerTag.php
@@ -10,17 +10,10 @@ declare(strict_types=1);
 
 namespace ActiveCollab\Retro\TemplatedUI;
 
-use ActiveCollab\Retro\UI\Renderer\RendererInterface;
 use ActiveCollab\TemplatedUI\Tag\Tag;
 
 class DividerTag extends Tag
 {
-    public function __construct(
-        private RendererInterface $renderer,
-    )
-    {
-    }
-
     public function render(): string
     {
         return '<sl-divider></sl-divider>';
